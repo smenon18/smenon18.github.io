5dd855485b1eaf7fa46139bfbee4bd999ec72a8a
Shaan Menon <smenon18@gmail.com>
2016-03-16 11:16:20 -0400

    changed brand link to my github repo, added a space for readability

--- js/navbar.js
+++ js/navbar.js
@@ -10,11 +10,11 @@
     var out = "<nav class='navbar navbar-inverse'>"; //Begin navbar
     out += "<div class='container'><div class='navbar-header'>";
     out += "<ul class='nav navbar-nav'>"; //Begin Content
-    out += "<li><a href='index.html' class='navbar-brand'>smenon18</a></li>"; //Brand
+    out += "<li><a href='http://github.com/smenon18' class='navbar-brand'>smenon18</a></li>"; //Brand
     out += "<li><a href='index.html'><div class='glyphicon glyphicon-home'></div>Home</a></li>"; //Index Link
     out += "<li><a href='projects.html'><div class='glyphicon glyphicon-file'></div>Projects</a></li>"; //Projects Link
     out += "<li><a href='about.html'><div class='glyphicon glyphicon-user'></div>About Me</a></li>"; //About Link
     out += "</ul>"; //End Content
     out += "</div></div></nav>"; //End navbar
-    document.getElementById('navbar-placeholder').innerHTML =out;
+    document.getElementById('navbar-placeholder').innerHTML = out;
  }
