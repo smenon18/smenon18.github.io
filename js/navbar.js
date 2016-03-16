112ac6438e6aac6a8daaddf9eacf815183adf03e
Shaan Menon <smenon18@gmail.com>
2016-03-16 11:10:10 -0400

    created navbar through js, to prevent writing it multiple times

---
+++ js/navbar.js
@@ -0,0 +1,20 @@
+/*
+ * @author Shaan Menon
+ * @date 16th Marh 2016
+ *
+ * This file is to load the navbar onto every page, preventing the necesity of writing
+ * it multiple times
+ */
+ 
+ this.onload = function(){
+    var out = "<nav class='navbar navbar-inverse'>"; //Begin navbar
+    out += "<div class='container'><div class='navbar-header'>";
+    out += "<ul class='nav navbar-nav'>"; //Begin Content
+    out += "<li><a href='index.html' class='navbar-brand'>smenon18</a></li>"; //Brand
+    out += "<li><a href='index.html'><div class='glyphicon glyphicon-home'></div>Home</a></li>"; //Index Link
+    out += "<li><a href='projects.html'><div class='glyphicon glyphicon-file'></div>Projects</a></li>"; //Projects Link
+    out += "<li><a href='about.html'><div class='glyphicon glyphicon-user'></div>About Me</a></li>"; //About Link
+    out += "</ul>"; //End Content
+    out += "</div></div></nav>"; //End navbar
+    document.getElementById('navbar-placeholder').innerHTML =out;
+ }
