96a2fb7ac66c737cb1f915fe77e04856ccffb2f1
Shaan Menon <smenon18@gmail.com>
2016-03-19 01:36:39 -0400

    added glyphicon-file back to projects part of navbar, tried more things to get dropdown to work

--- js/navbar.js
+++ js/navbar.js
@@ -12,7 +12,7 @@ this.onload = function(){
     out += "<ul class='nav navbar-nav'>"; //Begin Content
     out += "<li><a href='http://github.com/smenon18' class='navbar-brand'>Shaan Menon</a></li>"; //Brand
     out += "<li><a href='index.html'><div class='glyphicon glyphicon-home'></div>Home</a></li>"; //Index Link
-    out += "<li class='dropdown'><a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Projects<span class='caret'></span></a><ul class='dropdown-menu'><li><a href='projects.html#SoftwareEngineering'>Software Engineering</a></li><li><a href='projects.html#dotFiles'>dot Files</a></li><li><a href='projects.html#githubio'>smenon18.github.io</a></li></ul></li>"; //Projects Links
+    out += "<li class='dropdown'><a href='projects.html' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'><div class='glyphicon glyphicon-file'></div>Projects<span class='caret'></span></a><ul class='dropdown-menu'><li><a href='projects.html#SoftwareEngineering'>Software Engineering</a></li><li><a href='projects.html#dotFiles'>dot Files</a></li><li><a href='projects.html#githubio'>smenon18.github.io</a></li></ul></li>"; //Projects Links
     out += "<li><a href='about.html'><div class='glyphicon glyphicon-user'></div>About Me</a></li>"; //About Link
     out += "</ul>"; //End Content
     out += "</div></div></nav>"; //End navbar
